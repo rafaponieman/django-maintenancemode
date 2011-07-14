0055a80844238a043751a30854a69a42414ebeec
Rafael Ponieman <rafadev@gmail.com>
2011-07-14 09:37:28 -0300

    now still serving media, admin and admin media on maintenance mode

--- maintenancemode/middleware.py
+++ maintenancemode/middleware.py
@@ -3,24 +3,38 @@ from django.template import RequestContext
 from maintenancemode.http import Http503
 from maintenancemode.shortcuts import render_to_503
 from maintenancemode.conf.settings import MAINTENANCE_MODE
+import re
+
+HAS_LANG_PREFIX_RE = re.compile(r"^/(%s)/.*" % "|".join(map(lambda l: re.escape(l[0]), settings.CMS_LANGUAGES)))
+
+def has_lang_prefix(path):
+    check = HAS_LANG_PREFIX_RE.match(path)
+    if check is not None:
+        return check.group(1)
+    else:
+        return False
 
 class MaintenanceModeMiddleware(object):
     def process_request(self, request):
-        # Allow access if middleware is not activated
-        if not MAINTENANCE_MODE:
-            return None
+        if not request.path_info.startswith(settings.MEDIA_URL) and \
+            not request.path_info.startswith(settings.ADMIN_MEDIA_PREFIX) and \
+            not request.path_info.startswith('/admin'): #should be changed to be independent of where the admin is set up
 
-        # Allow access if remote ip is in INTERNAL_IPS
-        if request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS:
-            return None
-        
-        # Allow acess if the user doing the request is logged in and a
-        # staff member.
-        if hasattr(request, 'user') and request.user.is_staff:
-            return None
-        
-        # Otherwise show the user the 503 page
-        return self.process_exception(request, Http503())
+            # Allow access if middleware is not activated
+            if not MAINTENANCE_MODE:
+                return None
+    
+            # Allow access if remote ip is in INTERNAL_IPS
+            if request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS:
+                return None
+            
+            # Allow acess if the user doing the request is logged in and a
+            # staff member.
+            if hasattr(request, 'user') and request.user.is_staff:
+                return None
+            
+            # Otherwise show the user the 503 page
+            return self.process_exception(request, Http503())
 
     def process_exception(self,request,exception):
         if isinstance(exception, Http503):
